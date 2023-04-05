098d9eb8f8d8a5e5fa5a72ea78e956cd76fcefe4
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-04-05 13:12:28 +0530

    email valid

--- Employee.js
+++ Employee.js
@@ -1,16 +1,29 @@
-// Ensure @ and validate the
-// mandatory 2nd part i.e.
-// bridgelabz
-
-// Email UC 2
+// Ensure “.” after bridgelabz
+// and validate the
+// mandatory 3rd part i.e. co
 
 
-const emailRegex = /^([a-zA-Z0-9._%+-])+@(bridgelabz\.)+([a-zA-Z]{2,})$/;
-
-const email = 'abc.xyz@bridgelabz.co.in';
-
-if (emailRegex.test(email)) {
-  console.log('Email address is valid!');
-} else {
-  console.log('Email address is invalid.');
-}
+function validateEmail(email) {
+    // add a dot after "bridgelabz" if it doesn't exist
+    if (email.includes('bridgelabz') && !email.includes('.'))
+      email = email.replace('bridgelabz', 'bridgelabz.');
+  
+    // split the email into three parts: local part, domain part, and third part
+    const [localPart, domainPart, thirdPart] = email.split('@')[1].split('.');
+  
+    // check if the third part is "co"
+    if (thirdPart !== 'co')
+      return false;
+  
+    // validate the rest of the email using a regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+  
+  // example usage
+  const email1 = 'john.doe@bridgelabz.com.co';
+  const email2 = 'jane.smith@bridgelabz.co';
+  
+  console.log(validateEmail(email1)); // false
+  console.log(validateEmail(email2)); // true
+  
