c67f88ce97e9657ad4af1b7a1ed7a84d215b9580
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-04-05 13:11:13 +0530

    email valid

--- Employee.js
+++ Employee.js
@@ -1,19 +1,11 @@
-/*Validate Email address with a
-regex. The email consists of
-minimum 3 and optional 2 more
-parts with mandatory @ and .
-abc.xyz@bridgelabz.co.in
-Here abc, bridgelabz and co are
-mandatory and the remaining 2
-are optional
-To begin with lets validate the
-mandatory part and start with abc
+// Ensure @ and validate the
+// mandatory 2nd part i.e.
+// bridgelabz
 
-Email UC 1*/
+// Email UC 2
 
 
-
-const emailRegex = /^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/;
+const emailRegex = /^([a-zA-Z0-9._%+-])+@(bridgelabz\.)+([a-zA-Z]{2,})$/;
 
 const email = 'abc.xyz@bridgelabz.co.in';
 
