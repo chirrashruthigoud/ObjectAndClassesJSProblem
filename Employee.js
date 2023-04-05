55be3acd13f6c129640399400c9b5dd40a37a33f
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-04-05 13:15:36 +0530

    email valid

--- Employee.js
+++ Employee.js
@@ -1,19 +1,19 @@
-// Lets handle optional part
-// i.e. xyz in
-// abc.xyz@bridgelabz.co.in
-// NOTE: make sure only
-// following are valid special
-// characters _,+,
-// -,.
-// proceeding to xyz Email UC 4
+// Finally lets close the
+// expression with
+// supporting optional parts.
+// Note: Top Level Domains
+// (TLD) in the last part is the
+// optional country code and
+
+// its 2 characters only Email UC 5
 
 
-const email = 'abc.xyz@example.com'; // Replace with your email address
+const emailRegex = /^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})(\.[a-zA-Z]{2})?$/;
 
-const match = email.match(/^([a-zA-Z0-9._+-]+)@([a-zA-Z0-9-]+\.[a-zA-Z]{2,})$/);
-if (match) {
-  const xyz = match[1].split('.').pop();
-  console.log(xyz); // Outputs "xyz"
-} else {
-  console.log('Invalid email format');
-}
+const email1 = 'example@email.com';
+const email2 = 'example@email.co.uk';
+const email3 = 'example@email';
+
+console.log(emailRegex.test(email1)); // true
+console.log(emailRegex.test(email2)); // true
+console.log(emailRegex.test(email3)); // false
